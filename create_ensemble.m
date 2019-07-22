930b6d1889483c39cd9129c7a53cb3f1385b47b1
UnsignedByte <edlam1889@gmail.com>
2019-07-22 11:16:16 -0700

    fix ensemble creation

--- create_ensemble.m
+++ create_ensemble.m
@@ -1,21 +1,26 @@
-function ensemble = create_ensemble(num, endrange, percent, ensembledat, traitind) 
-    %{
-    endrange = double from 0-1: what fraction of total values from end/start are
-    considered valid choices
-    percent = what percent of values are part of the desired trait (0-1)
-    %}
-    valids = 1:size(ensembledat, 1)*endrange;
+function ensemble,data = create_ensemble(num, skew)
+
+    wcols = [1 3 4]; %which columns of data to save
+
+    if skew == -1
+        ensemble = ensembledat{1}(randperm(size(ensembledat{1},1),num-1), wcols);
+        ensemble(num,:) = ensembledat{2}(randi(size(ensembledat{2},1)), wcols);
+    elseif skew == 0
+        ensemble = ensembledat{1}(randperm(size(ensembledat{1},1),num/2), wcols);
+        ensemble(end+1:num,:) = ensembledat{2}(randperm(size(ensembledat{2},1),num/2), wcols);
+    elseif skew == 1
+        ensemble = ensembledat{2}(randperm(size(ensembledat{2},1),num-1), wcols);
+        ensemble(num,:) = ensembledat{1}(randi(size(ensembledat{1},1)), wcols);
+    end
     
-    respos = ensembledat(size(ensembledat, 1)-valids+1, [1 traitind]); %last n%, rated highest for trait
-    resneg = ensembledat(valids,[1 traitind]); %first n%, rated lowest for trait
+    ensemble = ensemble(randperm(num),:);
+    data = ensemble(:, 2:end)';
+    ensemble = ensemble(:,1)';
+    fnames = strcat('CFD-', ensemble, '-*-N.jpg');
+    ensemble = fullfile('CFD Version 2.0.3', 'CFD 2.0.3 Images', ensemble);
     
-    ensemble = cat(1,respos(randsample(size(respos, 1), floor(num*percent)),:),  resneg(randsample(size(resneg, 1), num-floor(num*percent)),:));
-    ensemble = ensemble(randperm(size(ensemble, 1)),:);
-    fnames = strcat('CFD-', ensemble(:,1), '-*-N.jpg');
-    ensemble(:,1) = fullfile('CFD Version 2.0.3', 'CFD 2.0.3 Images', ensemble(:,1));
-    
-    for i = 1:size(ensemble, 1)
-        imfile = dir(fullfile(ensemble{i,1}, fnames{i}));
-        ensemble{i, 1} = imread(fullfile(ensemble{i, 1},imfile(1).name));
+    for i = 1:num
+        imfile = dir(fullfile(ensemble{i}, fnames{i}));
+        ensemble{i} = imread(fullfile(ensemble{i},imfile(1).name));
     end
 end
