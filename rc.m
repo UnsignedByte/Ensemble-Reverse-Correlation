034bc72c5acfd6c5bc153c36e5551cc24efba36c
UnsignedByte <edlam1889@gmail.com>
2019-07-24 10:13:16 -0700

    Update rc.m

--- rc.m
+++ rc.m
@@ -18,6 +18,8 @@ baseImg = rgb2gray(imread('male.jpg'));
 trials = 10; %100
 
 siz = size(baseImg, 1);
+sk = [-1 0]; %which skews included
+nsk = size(sk,2); %number of types of skews
 
 %% Check for and Load Ensemble Data
 
@@ -27,8 +29,8 @@ else
     gen_ensembledat;
 end
 
-ensembles = cell(3, trials);
-ensembledata = cell(3, trials);
+ensembles = cell(nsk, trials);
+ensembledata = cell(nsk, trials);
 
 noises = cell(trials,1);
 
@@ -39,20 +41,17 @@ for i = 1:trials
     Screen('Flip', window);
     noises{i} = generate_noise(siz);
 end
-noises = repmat(noises, 1,3)';
-for i = 1:3
+noises = repmat(noises, 1,nsk)';
+for i = 1:nsk
     noises(i,:) = noises(i, randperm(trials));
-end
-
-for i = -1:1
     for j = 1:trials
-        [ensembles{i+2,j},ensembledata{i+2,j}] = create_ensemble(ensembledat, num, i);
+        [ensembles{i,j},ensembledata{i,j}] = create_ensemble(ensembledat, num, sk(i));
     end
 end
 
 %% Generate Textures
 
-ord = randperm(3*trials);
+ord = randperm(nsk*trials);
 
 w = ww/4;
 h = w*1718/2444;
@@ -61,10 +60,10 @@ mask = imread('ovalmask.jpg');
 mask = imresize(mask, size(ensembles{1,1}{1}(:,:,1)));
 mask = mask(:,:,1);
 
-tid = cell(3*trials, num);
-for i = 1:3*trials
+tid = cell(nsk*trials, num);
+for i = 1:nsk*trials
     for j = 1:num
-        DrawFormattedText(window, ['Making Textures...\n' num2str(floor(((i-1)*num+j)/trials/num/3*100)) '%'], 'center', 'center');
+        DrawFormattedText(window, ['Making Textures...\n' num2str(floor(((i-1)*num+j)/trials/num/nsk*100)) '%'], 'center', 'center');
         Screen('Flip', window);
         a = ensembles{ceil(ord(i)/trials),mod(ord(i),trials)+1}{j};
         a(:,:,4) = mask;
@@ -74,7 +73,6 @@ end
 res = zeros(siz,siz,trials); %what they choose
 inv = zeros(siz,siz,trials); %what they dont
 
-RestrictKeysForKbCheck([KbName('f'), KbName('j')]); %Restrict to f and j keys
 
 
 radius = wh/3;
@@ -89,7 +87,7 @@ delay1 = 0.5; %time btwn ensemble and rc
 delay2 = 1; %time of crosshair
 delay3 = 3;
 
-chosen = ones(3, trials); %List of chosen thingse
+chosen = ones(nsk, trials); %List of chosen thingse
 
 cross = [-ww/100 ww/100 0 0;0 0 -ww/100 ww/100];
 crossW = wh/500;
@@ -103,24 +101,35 @@ Screen('TextSize',window, 30);
 Screen('TextStyle', window, 0);
 
 DrawFormattedText(window, ...
-    'You will have 300 trials to complete.', ...
+    ['You will have ' num2str(trials*nsk) ' trials to complete.\nThe following trial will be an example with instructions.\nPress any key to continue.'], ...
     'center',300);
 Screen('Flip', window);
 
-WaitSecs(delay3);
+KbStrokeWait();
+
+curNoise = generate_noise(siz);
+ims = (cat(3, min(uint8(double(baseImg) + curNoise),255), min(uint8(double(baseImg) - curNoise),255)));
 
 DrawFormattedText(window,...
-    'You will be shown various images based off of this picture.', ...
+    'Focus on the crosshair in the center.', ...
+    'center',300);
+Screen('DrawLines', window, cross, crossW, 0, [ww/2 wh/2], 2);
+Screen('Flip', window, 0, 0);
+WaitSecs(delay2);
+curEnsemble = cell2mat(tid(t,:)');
+
+curNoise = noises{ceil(ord(t)/trials),mod(ord(t),trials)+1};
+
+Screen('DrawTextures', window, curEnsemble, [], coordinates); % display in grid
+Screen('Flip', window);
+WaitSecs(ens_time);
+Screen('Flip', window);
+WaitSecs(delay1);
+
+DrawFormattedText(window,...
+    'Choose the image that appears more trustworthy out of the 2 shown.', ...
     'center',300);
 Screen('DrawTexture', window, showBaseImg, [], [(ww-siz)/2, (wh-siz)/2, (ww+siz)/2, (wh+siz)/2]);
-Screen('Flip', window);
-
-WaitSecs(delay3);
-
-DrawFormattedText(window,...
-    'Choose which Image appears more trustworthy, out of the 2 you are shown.', ...
-    'center',300);
-
 Screen('Flip', window);
 
 WaitSecs(delay3);
@@ -134,15 +143,15 @@ Screen('Flip', window);
 WaitSecs(delay3);
 
 %% Run Trials
+RestrictKeysForKbCheck([KbName('f'), KbName('j')]); %Restrict to f and j keys
 
-for t = 1:3*trials
+for t = 1:nsk*trials
     Screen('DrawLines', window, cross, crossW, 0, [ww/2 wh/2], 2);
-    Screen('Flip', window);
+    Screen('Flip', window, 0, 0);
     WaitSecs(delay2);
     curEnsemble = cell2mat(tid(t,:)');
 
     curNoise = noises{ceil(ord(t)/trials),mod(ord(t),trials)+1};
-    Screen('DrawLines', window, cross, crossW, 0, [ww/2 wh/2], 2);
 
     Screen('DrawTextures', window, curEnsemble, [], coordinates); % display in grid
     Screen('Flip', window);
